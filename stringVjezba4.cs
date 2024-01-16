87a7a404cb4cc81257ca5ab79e60b7019d3a89f6
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-01-16 13:00:00 +0100

    Add files via upload

---
+++ stringVjezba4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("upisi neku recenicu:");
+            string s1 = Console.ReadLine();
+            string[]rijeci = s1.Split(' ');
+            string prvarijec = rijeci[0];
+            string zadnjarijec = rijeci[rijeci.Length - 1];
+            
+            Console.WriteLine("prva rijec" + prvarijec);
+            Console.WriteLine("druga rijec" + zadnjarijec);
+            Console.ReadKey();
+        }
+    }
+}
